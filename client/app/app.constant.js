d02de796fbd47700163b637bd06a28505d2dcd53
Alex Mercier <pro.alexandre.mercier@gmail.com>
2016-03-12 22:37:38 -0800

    Ran gulp serve

---
+++ client/app/app.constant.js
@@ -0,0 +1,13 @@
+(function(angular, undefined) {
+  angular.module("nodeJsOpenShiftSandboxApp.constants", [])
+
+.constant("appConfig", {
+	"userRoles": [
+		"guest",
+		"user",
+		"admin"
+	]
+})
+
+;
+})(angular);
